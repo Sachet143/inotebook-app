fb86e5448291360a0509401b81161ac3c3c69785
Sachet Shrestha <revenger.stha@gmail.com>
2023-08-10 09:24:22 +0545

    video 44 completed

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,14 @@ const port = 3000;
 // Middlewares
 app.use(express.json())
 
+// Available Routes
+app.use('/api/auth', require('./routes/auth.js'))
+app.use('/api/notes', require('./routes/notes.js'))
+
 app.get('/', (req, res) => {
   res.send('Hello Sachet!')
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
