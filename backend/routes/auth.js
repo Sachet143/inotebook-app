676c1ec0c2c241b162aaa6c008dd48499e44703f
Sachet Shrestha <revenger.stha@gmail.com>
2023-08-10 09:45:37 +0545

    video 45 completed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const User = require('../models/User');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    let obj = {
-        a: 'sachet',
-    }
-    res.json(obj);
-})
+// Create a user using: POST "/api/auth". Doesn't required Auth
+router.post('/createuser', (req, res) => {
+
+    console.log(req.body);
+    const user = User(req.body);
+    user.save();
+    res.send(req.body);
+});
 
 module.exports = router;
